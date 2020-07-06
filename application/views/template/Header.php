3b12b94b7e4586ae051426763c8e0aaaaeb843e2
suhada budi setiawan <setiawanhada46@gmail.com>
2020-07-06 21:03:39 +0700

    template ci front end bizland

---
+++ application/views/template/Header.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<title>BizLand Bootstrap Template - Index</title>
+	<meta content="" name="descriptison">
+	<meta content="" name="keywords">
+
+	<!-- Favicons -->
+	<link href="<?= base_url('')?>assets/img/favicon.png" rel="icon">
+	<link href="<?= base_url('')?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+	<link
+		href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+		rel="stylesheet">
+
+	<!-- Vendor CSS Files -->
+	<link href="<?= base_url('')?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?= base_url('')?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+	<link href="<?= base_url('')?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="<?= base_url('')?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="<?= base_url('')?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+	<link href="<?= base_url('')?>assets/vendor/aos/aos.css" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+	<link href="<?= base_url('')?>assets/css/style.css" rel="stylesheet">
+
+	<!-- =======================================================
+  * Template Name: BizLand - v1.0.1
+  * Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+</head>
+
+<body>
+
+	<!-- ======= Top Bar ======= -->
+	<div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+		<div class="container d-flex">
+			<div class="contact-info mr-auto">
+				<i class="icofont-envelope"></i> <a href="mailto:contact@example.com">contact@example.com</a>
+				<i class="icofont-phone"></i> +1 5589 55488 55
+			</div>
+			<div class="social-links">
+				<a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+				<a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+				<a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+				<a href="#" class="skype"><i class="icofont-skype"></i></a>
+				<a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+			</div>
+		</div>
+	</div>
+
+	<!-- ======= Header ======= -->
+	<header id="header" class="fixed-top">
+		<div class="container d-flex align-items-center">
+
+			<h1 class="logo mr-auto"><a href="index.html">BizLand<span>.</span></a></h1>
+			<!-- Uncomment below if you prefer to use an image logo -->
+			<!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt=""></a>-->
+
+			<nav class="nav-menu d-none d-lg-block">
+				<ul>
+					<li class="active"><a href="index.html">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#services">Services</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#team">Team</a></li>
+					<li class="drop-down"><a href="">Drop Down</a>
+						<ul>
+							<li><a href="#">Drop Down 1</a></li>
+							<li class="drop-down"><a href="#">Deep Drop Down</a>
+								<ul>
+									<li><a href="#">Deep Drop Down 1</a></li>
+									<li><a href="#">Deep Drop Down 2</a></li>
+									<li><a href="#">Deep Drop Down 3</a></li>
+									<li><a href="#">Deep Drop Down 4</a></li>
+									<li><a href="#">Deep Drop Down 5</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Drop Down 2</a></li>
+							<li><a href="#">Drop Down 3</a></li>
+							<li><a href="#">Drop Down 4</a></li>
+						</ul>
+					</li>
+					<li><a href="#contact">Contact</a></li>
+
+				</ul>
+			</nav><!-- .nav-menu -->
+
+		</div>
+	</header><!-- End Header -->
+
+	<!-- ======= Hero Section ======= -->
+	<section id="hero" class="d-flex align-items-center">
+		<div class="container" data-aos="zoom-out" data-aos-delay="100">
+			<h1>Welcome to <span>BizLand</spa>
+			</h1>
+			<h2>We are team of talanted designers making websites with Bootstrap</h2>
+			<div class="d-flex">
+				<a href="#about" class="btn-get-started scrollto">Get Started</a>
+				<a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox btn-watch-video"
+					data-vbtype="video" data-autoplay="true"> Watch Video <i class="icofont-play-alt-2"></i></a>
+			</div>
+		</div>
+	</section><!-- End Hero -->
